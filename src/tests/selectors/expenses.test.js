d9af37d93911bae26d0d6efee199e82ba074c2a8
Felipe Leão <felipe@felipeleao.com>
2018-07-02 22:54:21 -0300

    chore: Create tests for selectors

---
+++ src/tests/selectors/expenses.test.js
@@ -0,0 +1,92 @@
+import moment from 'moment'
+import selectExpenses from '../../selectors/expenses'
+
+const expenses = [{
+  id: '1',
+  description: 'Rent',
+  note: '',
+  amount: 5000,
+  createdAt: 0
+}, {
+  id: '2',
+  description: 'Car',
+  note: 'Broken',
+  amount: 100,
+  createdAt: moment(0).subtract(4, 'days').valueOf()
+}, {
+  id: '3',
+  description: 'Credit Card',
+  note: '',
+  amount: 8900,
+  createdAt: moment(0).add(4, 'days').valueOf()
+}]
+
+test('should filter by text value', () => {
+  const filters = {
+    text: 'e',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  }
+
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([
+    expenses[2], expenses[0]
+  ])
+})
+
+test('should filter by startDate', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: moment(0),
+    endDate: undefined
+  }
+
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([
+    expenses[2], expenses[0]
+  ])
+})
+
+test('should filter by endDate', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: moment(0).add(2, 'days')
+  }
+
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([
+    expenses[0], expenses[1]
+  ])
+})
+
+test('should sort by date', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  }
+
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([
+    expenses[2], expenses[0], expenses[1]
+  ])
+})
+
+test('should sort by amount', () => {
+  const filters = {
+    text: '',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  }
+
+  const result = selectExpenses(expenses, filters)
+  expect(result).toEqual([
+    expenses[2], expenses[0], expenses[1]
+  ])
+})
