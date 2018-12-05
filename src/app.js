90692f463f6ffd263c238ec09a357b91112b82bb
Felipe Leão <felipe@felipeleao.com>
2018-12-05 18:34:42 -0200

    add form

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
 import AppRouter from "./routers/AppRouter"
+import configureStore from "./store/configureStore"
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+import {addExpense} from "./actions/expenses"
+import {setTextFilter} from "./actions/filters"
+import getVisibleExpenses from "./selectors/expenses"
+
+const store = configureStore()
+store.dispatch(addExpense({description: 'Water', amount: 2}))
+store.dispatch(addExpense({description: 'Gas', amount: 3}))
+store.dispatch(setTextFilter('wat'))
+console.log(store.getState())
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider> 
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
