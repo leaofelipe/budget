31ad5d3117a9a165151b2114b45f9a51f4c7efec
Felipe Leão <felipe@felipeleao.com>
2018-12-07 19:26:57 -0200

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyCLT5Gs5N4blaoRXoCy7eVePk8Gtkr3iLQ",
+  authDomain: "budget-leao.firebaseapp.com",
+  databaseURL: "https://budget-leao.firebaseio.com",
+  projectId: "budget-leao",
+  storageBucket: "budget-leao.appspot.com",
+  messagingSenderId: "1001389880164"
+};
+
+firebase.initializeApp(config);
