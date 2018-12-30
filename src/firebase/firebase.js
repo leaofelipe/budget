786cbaf89e9ec4d8ca3a93ca7d88aff8d8d097be
Felipe Leão <felipe@felipeleao.com>
2018-12-30 21:27:40 -0200

    Create Asyncronous files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,15 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyCLT5Gs5N4blaoRXoCy7eVePk8Gtkr3iLQ",
-  authDomain: "budget-leao.firebaseapp.com",
-  databaseURL: "https://budget-leao.firebaseio.com",
-  projectId: "budget-leao",
-  storageBucket: "budget-leao.appspot.com",
-  messagingSenderId: "1001389880164"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER
 };
 
 firebase.initializeApp(config);
+const database = firebase.database()
+
+export {firebase, database as default}
