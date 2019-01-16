265b8735f8582f865ebe5c8915b515de038f1e9f
Felipe Leão <felipe@felipeleao.com>
2019-01-16 12:14:48 -0200

    Add EditExpense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -2,17 +2,16 @@ import React from  'react'
 import {shallow} from 'enzyme'
 import { EditExpensePage } from '../../components/EditExpensePage'
 import expenses from '../fixtures/expenses'
-import {EditExpenses} from '../../components/EditExpensePage'
 
-let editExpense, startRemoveExpense, history, wrapper
+let startEditExpense, startRemoveExpense, history, wrapper
 
 beforeEach(() => {
-  editExpense = jest.fn()
+  startEditExpense = jest.fn()
   startRemoveExpense = jest.fn()
   history = {push: jest.fn()}
   wrapper = shallow(
     <EditExpensePage
-      editExpense={editExpense}
+      startEditExpense={startEditExpense}
       startRemoveExpense={startRemoveExpense}
       history={history}
       expense={expenses[2]}
@@ -27,7 +26,7 @@ test('should render EditExpensePage correctly', () => {
 test('should handle editExpense', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2])
   expect(history.push).toHaveBeenLastCalledWith('/')
-  expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2])
+  expect(startEditExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2])
 })
 
 test('should handle startRemoveExpense', () => {
